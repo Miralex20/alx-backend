ce37225b720106b601cac05f2ff9349ead39983c
Ohuka Miracle Chigozie <123806072+Miralex20@users.noreply.github.com>
2024-11-03 13:50:57 +0100

    backend task

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
-"""A module on caching task"""
-BaseCaching = __import__("base_caching").BaseCaching
+"""Basic caching module.
+"""
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ A caching system that inherits from base-caching"""
+    """Represents an object that allows storing and
+    retrieving items from a dictionary.
+    """
     def put(self, key, item):
-        """ Assign item to a key in a dictionary"""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
-        """ return the value linked to a key"""
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
